ffc116cbe0badd71de3a3f7456d04db62adb0ac6
Bogdan Temnik <bogdantemnikconnect@gmail.com>
2024-12-15 23:02:45 +0100

    add first layout

---
+++ inc/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>todo app</title>
+
+        <link rel="stylesheet" href="styles.css"/>
+    </head>
+    <body>
+        <div class="navbar">
+            <div id="navbar-left">
+                <div class="navbar-element"><a href="#">Home</a></div>
+                <div class="navbar-element"><a href="#">About</a></div>
+                <div class="navbar-element"><a href="#">Settings</a></div>
+            </div>
+            <div id="navbar-right">
+                <div class="navbar-element"><a href="#">Donate</a></div>
+                <div class="navbar-element"><a href="#">Feeling lucky?</a></div>
+            </div>
+        </div>
